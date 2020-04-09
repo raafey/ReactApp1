2ad46dc5de079194b941e6de0384133e49eeaa39
Muhammad Raafey <raafeytariq@gmail.com>
2020-04-09 20:38:08 +0500

    uploaded file

---
+++ Raafey.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Raafey extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Muhammad Raafey Tariq i16-0259</h1>
+        <body> 
+          <img src="https://i.pinimg.com/originals/4e/02/27/4e0227920cd7f5f4312e374d4e06b667.jpg"  width="1280" height="720"/> 
+        </body>
+      </div>
+    ); 
+  }
+}
+
+export default Raafey;
